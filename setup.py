7292a1494830326a967515cd4452302d00a3e1cf
Philipp Reitter <p.reitter@arivo.co>
2025-06-14 16:31:43 +0200

    version bump [skip ci]

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-VERSION = "1.8.0"
+VERSION = "1.8.1"
 
 install_requires = []
 tests_require = []
